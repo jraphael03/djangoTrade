83f7848e46272ae7b4aedda7939f27e527fb1a4f
jraphael03 <62814648+jraphael03@users.noreply.github.com>
2020-11-23 14:31:34 -0500

    worked form so it can accept submissions

--- stock/views.py
+++ stock/views.py
@@ -5,15 +5,25 @@ def home( request):
     import requests
     import json
     
-    #pk_d498d40ec96e4e1d8bb7a6e68424254d   (iexstockAPIkey)
-    api_request = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_d498d40ec96e4e1d8bb7a6e68424254d")
+    if request.method == "POST":
+        ticker = request.POST['ticker']   #ticker is the name of the form so whatever this is the form inputs
+        #pk_d498d40ec96e4e1d8bb7a6e68424254d   (iexstockAPIkey)
+        api_request = requests.get("https://cloud.iexapis.com/stable/stock/" + ticker +"/quote?token=pk_d498d40ec96e4e1d8bb7a6e68424254d")
+            
+        try:
+            api = json.loads(api_request.content)
+        except Exception as e:
+            api = "Error..."
+        return render( request, 'home.html', {'api' : api})
 
-    try:
-        api = json.loads(api_request.content)
-    except Exception as e:
-        api = "Error..."
+    else:
+        return render( request, 'home.html', {'ticker' : 'Enter a ticker symbol above'})
 
-    return render( request, 'home.html', {'api' : api})
+
+
+
+
+
 
 def about( request):
     return render( request, 'about.html', {})
