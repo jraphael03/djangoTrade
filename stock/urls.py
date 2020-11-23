9c75b623ed81ddd6f25c90113ca50faf7dd9ace5
jraphael03 <62814648+jraphael03@users.noreply.github.com>
2020-11-23 11:37:40 -0500

    corrected links using names in urls.py

--- stock/urls.py
+++ stock/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),   #localhost:8000/
+    path('', views.home, name='home'),   #localhost:8000/  the name is used for links
     path('about', views.about, name='about'),    #localhost:8000/about
 ]
