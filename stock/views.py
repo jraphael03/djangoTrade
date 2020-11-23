372e3a92043a0cfaf0fa348d6108710692d1c74f
jraphael03 <62814648+jraphael03@users.noreply.github.com>
2020-11-23 11:58:48 -0500

    added apikey hard coded apple stock and displayed code on screen

--- stock/views.py
+++ stock/views.py
@@ -2,7 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 def home( request):
-    return render( request, 'home.html', {})
+    import requests
+    import json
+    
+    #pk_d498d40ec96e4e1d8bb7a6e68424254d   (iexstockAPIkey)
+    api_request = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_d498d40ec96e4e1d8bb7a6e68424254d")
+
+    try:
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = "Error..."
+
+    return render( request, 'home.html', {'api' : api})
 
 def about( request):
     return render( request, 'about.html', {})
